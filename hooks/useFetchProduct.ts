d2c8f4a77b7f9235ea08cf2b7f2132092231558d
dyv21 <denis@artkerr.com>
2025-02-03 17:56:46 +0300

    fix: issue with no brands goods

--- hooks/useFetchProduct.ts
+++ hooks/useFetchProduct.ts
@@ -1,35 +1,36 @@
-import {useEffect, useState} from "react"
-import axios from "axios"
-import {useRouter} from "next/router"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export type ProductType = {
-  id: string
-  title: string
-  description: string
-  thumbnail: string
-  price: number
-  brand: string
-  rating: number
-  stock: number
-  images: []
-}
+  id: string;
+  title: string;
+  description: string;
+  thumbnail: string;
+  price: number;
+  brand: string;
+  rating: number;
+  stock: number;
+  images: [];
+};
 
-export type Nullable<T> = null | T
+export type Nullable<T> = null | T;
 
 export const useFetchProduct = (): Nullable<ProductType> => {
-  const [product, setProduct] = useState<Nullable<ProductType>>(null)
+  const [product, setProduct] = useState<Nullable<ProductType>>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-  const router = useRouter()
-
+  const router = useRouter();
 
   useEffect(() => {
-    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/products/${2}`)
-      .then(res => {
-        setProduct(res.data)
-
-      })
-      .catch(err => console.log(err));
+    setIsLoading(true);
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_URL}/products/${router.query.id}`)
+      .then((res) =>  setProduct(res.data))
+      .catch((err) => setError(err.message))
+      .finally(() => setIsLoading(false));
   }, []);
 
   return product;
-}
+};
