7ada85a32f18c0c6a15bad22d18f380da6897c0a
dyv21 <denis@artkerr.com>
2025-02-03 08:17:56 +0300

    Init

---
+++ config/site.ts
@@ -0,0 +1,61 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "EcomApp",
+  description: "",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Catalog",
+      href: "/catalog",
+    },
+    {
+      label: "Brands",
+      href: "/brands",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
