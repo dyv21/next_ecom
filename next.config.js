79c1fd54a05dd7520ce6ccdc1de940c51c348850
dyv21 <denis@artkerr.com>
2025-02-11 12:34:07 +0300

    fix: name exports errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/next_ecom",
+  assetPrefix: "/next_ecom",
   trailingSlash: true,
   output: "export",
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
