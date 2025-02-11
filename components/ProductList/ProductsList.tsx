9c263bdc26ade182dbea90e197f9b30aef0c1ac3
dyv21 <denis@artkerr.com>
2025-02-11 10:48:55 +0300

    feat: add getStaticProps for ProductList component

--- components/ProductList/ProductsList.tsx
+++ components/ProductList/ProductsList.tsx
@@ -1,42 +1,53 @@
 import React from "react";
 import { cn } from "@heroui/react";
 import ProductListItem from "@/components/ProductItem/ProductItem";
-import { useFetchGoods } from "@/hooks/useFetchGoods";
+import { ProductThumbType, useFetchGoods } from "@/hooks/useFetchGoods";
 import SkeletonComponent from "@/components/Skeleton/Skeleton";
 
-export type ProductGridProps = React.HTMLAttributes<HTMLDivElement> & {
-  itemClassName?: string;
+export type ProductListProps = React.HTMLAttributes<HTMLDivElement> & {
+  goodsList: ProductThumbType[];
 };
 
-const ProductsList = React.forwardRef<HTMLDivElement, ProductGridProps>(
-  ({ itemClassName, className, ...props }, ref) => {
-    const { goodsList, loading, error } = useFetchGoods();
-    {
-      if (error) return <p>Ошибка: {error}</p>;
-    }
+const ProductsList = React.forwardRef<HTMLDivElement, ProductListProps>(
+  (props, ref) => {
+    const { goodsList } = props;
+
+    // const { goodsList, loading, error } = useFetchGoods();
+    // {
+    //   if (error) return <p>Ошибка: {error}</p>;
+    // }
 
     return (
       <div
         ref={ref}
         className={cn(
           "grid w-full grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4",
-          className,
         )}
         {...props}
       >
-        {loading
-          ? Array.from({ length: 20 })
-              .fill(null)
-              .map((_, i) => <SkeletonComponent />)
-          : goodsList &&
-            goodsList.map((product) => (
-              <ProductListItem
-                key={product.id}
-                removeWrapper
-                {...product}
-                className={cn("w-full snap-start", itemClassName)}
-              />
-            ))}
+        {/*{loading*/}
+        {/*  ? Array.from({ length: 20 })*/}
+        {/*      .fill(null)*/}
+        {/*      .map((_, i) => <SkeletonComponent />)*/}
+        {/*  : goodsList &&*/}
+        {/*    goodsList.map((product) => (*/}
+        {/*      <ProductListItem*/}
+        {/*        key={product.id}*/}
+        {/*        removeWrapper*/}
+        {/*        {...product}*/}
+        {/*        className={cn("w-full snap-start", itemClassName)}*/}
+        {/*      />*/}
+        {/*    ))}*/}
+
+        {goodsList &&
+          goodsList.map((product) => (
+            <ProductListItem
+              key={product.id}
+              removeWrapper
+              {...product}
+              className={cn("w-full snap-start")}
+            />
+          ))}
       </div>
     );
   },
--- pages/catalog/index.tsx
+++ pages/catalog/index.tsx
@@ -1,16 +1,34 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import ProductsList from "@/components/ProductList/ProductsList";
+import { API } from "@/shared/api/api";
+import { ProductThumbType } from "@/hooks/useFetchGoods";
 
-export default function DocsPage() {
+export const getStaticProps = async () => {
+  const res = await API.getGoods();
+  return {
+    props: {
+      goodsList: res.data.products,
+    },
+  };
+};
+
+type PropsType = {
+  goodsList: ProductThumbType[];
+};
+
+const Catalog = (props: PropsType) => {
+  const { goodsList } = props;
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block text-center justify-center">
           <h1 className={title({ color: "violet" })}>Catalog</h1>
-          <ProductsList />
+          <ProductsList goodsList={goodsList} />
         </div>
       </section>
     </DefaultLayout>
   );
-}
+};
+
+export default Catalog;
