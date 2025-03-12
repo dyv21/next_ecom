7aa0f1d1d35cacf1698baa1d12d89b033dd03f74
dyv21 <denis@artkerr.com>
2025-03-12 20:15:42 +0300

    add authSlice

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authReducer } from "@/redux/authSlice";
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { auth: authReducer },
 });
